1699d93180a8d2a59d30d61867b3b882277e0dd1
arnaudram <52896739+arnaudram@users.noreply.github.com>
2020-03-20 13:51:09 -0700

    task 3, feat: implementing sharepreference to save textview_name value

--- app/src/main/java/com/example/learningkotlin/WelcomeActivity.kt
+++ app/src/main/java/com/example/learningkotlin/WelcomeActivity.kt
@@ -1,19 +1,29 @@
 package com.example.learningkotlin
 
+import android.app.Activity
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : AppCompatActivity() {
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
-
-
+        val sharepref= getSharedPreferences("user_preference", Context.MODE_PRIVATE)?: return
+       with(sharepref.edit()){
+           this.putString("name_pref",receive_extra_name)
+           this.commit()
+       }
+      tv_namePreference.text=sharepref.getString("name_pref","")
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -23,19 +33,28 @@ class WelcomeActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-       when(item.itemId){
+        return when(item.itemId){
             R.id.profile_name ->{
                 welcomeToast()
-              return  true
+
+                true
             }
-            else->{return super.onOptionsItemSelected(item) }
+            else->{
+                super.onOptionsItemSelected(item)
+            }
         }
 
 
     }
 
+    private val receive_extra_name: String
+        get() {
+            val receive_extra_name = intent.getStringExtra(MainActivity().NAME_MAINACTIVITY)!!
+            return receive_extra_name
+        }
+
     private fun welcomeToast() {
-        val receive_extra_name= intent.getStringExtra(MainActivity().NAME_MAINACTIVITY)!!
+       //val receive_extra_name= intent.getStringExtra(MainActivity().NAME_MAINACTIVITY)!!
         Toast.makeText(baseContext,"Welcome :$receive_extra_name ",Toast.LENGTH_LONG).show()
     }
 }
